866f4f512e65d575aba64e3d8337cb92d5cb91d1
Emptyset <21324784@qq.com>
2016-02-26 06:48:09 +0800

    add Config

---
+++ dHydra/config/config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf8 -*-
+"""
+Config类
+Created on 02/22/2016
+@description:	Used for 
+@author: 		Wen Gu
+@contact: 		emptyset110@gmail.com
+"""
+import const
+class Config:
+
+	def __init__(self):
+		self.VERSION = const.VERSION
+
+	def reset(self):
+		self.__init__()
+		print "The configuration has been reset"
---
+++ dHydra/config/const.py
@@ -0,0 +1,17 @@
+# -*- coding: utf8 -*-
+"""
+const常量
+Created on 02/22/2016
+@author: Wen Gu
+@contact: emptyset110@gmail.com
+"""
+VERSION = '0.0.1'
+
+#	路径配置		
+#	Path Configuration
+PATH_DATA_ROOT = '../data/'
+PATH_DATA_REALTIME = 'stock_realtime/'
+
+#	Mongodb连接配置
+#	Mongodb Configuration
+
