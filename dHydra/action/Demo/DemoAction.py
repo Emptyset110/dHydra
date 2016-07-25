54f56ffa992c36d0543bcab6bc8d01776ca400de
Emptyset <21324784@qq.com>
2016-07-26 04:20:48 +0800

    修复无法开启多个action的bug

--- dHydra/action/Demo/DemoAction.py
+++ dHydra/action/Demo/DemoAction.py
@@ -18,23 +18,9 @@ from dHydra.core.Functions import *
 # 以上是自动生成的 #
 class DemoAction(Action):
 	def __init__(self, name, **kwargs):
-		# 用户自定义自动加载的_producer_list
-		# 这一部分已经统一规定写进入口app.py
-		# self._producer_list = [
-		# 	}
-		# 		"name"	:	"Demo"
-		# 	,	"producer_name"	:	"Demo.Demo"		#这是在action内部给producer起的自定义名字，可随意。一般最好遵守<actionName.producerName>
-		# 	}
-		# ]
-		# 设置进程检查消息队列的间隔
-		self._interval = 0.5
 		super().__init__(name, **kwargs)
 		print(self._name,"初始化")
 
 	# 需要重写的方法
-	def handler(self):
-		while not self._queue.empty():
-			self._lock.acquire()
-			event = self._queue.get(True)
-			self._lock.release()
-			print("DemoAction:{}".format( event.data ) )
+	def handler(self, event):
+		print("DemoAction:", event.data)
